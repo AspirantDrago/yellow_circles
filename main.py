8eb84c978cf11f6cfbfc0c71cabf83616a326fa4
Aleksandr <sanekclubstr@mail.ru>
2020-11-26 20:57:22 +0400

    Change UI with new ui-file

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import random
 
 from PyQt5.QtCore import QPoint
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5 import uic
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         self.WIDTH = 300
@@ -14,10 +15,7 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, self.WIDTH, self.HEIGHT)
-        self.btn = QPushButton('Создать окружность', self)
-        self.btn.resize(self.WIDTH - 40, 40)
-        self.btn.move(20, self.HEIGHT - 40 - 10)
+        uic.loadUi('UI.ui', self)
         self.btn.clicked.connect(self.btn_click)
 
     def btn_click(self):
